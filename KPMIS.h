d6b7093a447b582e6a5b4f6cddc4f87295831745
strix190788 <strix190788@gmail.com>
2026-02-11 13:16:36 +0400

    Create KPMIS.h

---
+++ KPMIS.h
@@ -0,0 +1,75 @@
+
+#include <Arduino.h>
+
+class KPMIS {
+public:
+  KPMIS() {}
+  void initMotors() {
+    pinMode(motor[0][0], OUTPUT);
+    pinMode(motor[1][0], OUTPUT);
+    pinMode(motor[1][1], OUTPUT);
+    pinMode(motor[0][1], OUTPUT);
+    digitalWrite(motor[0][0], motor[0][2]);
+    digitalWrite(motor[1][0], motor[1][2]);
+	for (int i = 0; i < 6; i++) pinMode(led[i], OUTPUT);
+  }
+  void reverse() {
+    for (int i = 0; i < 3; i++) {
+      byte a = motor[0][i];
+      motor[0][i] = motor[1][i];
+      motor[1][i] = a;
+    }
+    reverseMotorNumber(0);
+    reverseMotorNumber(1);
+  }
+
+  void reverseMotorNumber(bool motorNumber) {
+    motor[motorNumber][2] = not motor[motorNumber][2];
+    digitalWrite(motor[motorNumber][0], motor[motorNumber][2]);
+  }
+
+  void stop() {
+    digitalWrite(motor[0][1], 0);
+    digitalWrite(motor[1][1], 0);
+    delay(50);
+  }
+
+  void move(int pwrLeft, int pwrRight) {
+    pwrLeft = constrain(pwrLeft, -255, 255);
+    pwrRight = constrain(pwrRight, -255, 255);
+    if (pwrLeft > 0) digitalWrite(motor[0][0], motor[0][2]);
+    else digitalWrite(motor[0][0], not motor[0][2]);
+    if (pwrRight > 0) digitalWrite(motor[1][0], motor[1][2]);
+    else digitalWrite(motor[1][0], not motor[1][2]);
+    analogWrite(motor[0][1], abs(pwrLeft));
+    analogWrite(motor[1][1], abs(pwrRight));
+  }
+  void move(byte pwrLeft, byte pwrRight, int millsec) {
+    move(pwrLeft, pwrRight);
+    delay(millsec);
+    stop();
+  }
+
+  bool readButton(byte buttonNumber) {
+    buttonNumber = constrain(buttonNumber, 0, 5);
+    return digitalRead(button[buttonNumber]);
+  }
+
+  int readPot(byte potNumber) {
+    potNumber = constrain(potNumber, 0, 5);
+    return analogRead(pot[potNumber]);
+  }
+
+  void ledValue(byte ledNumber, bool state) {
+    digitalWrite(led[ledNumber], state);
+  }
+private:
+  byte motor[2][3] = {
+    { 45, 44, 0 },  // 0
+    { 47, 46, 0 }   // 1
+  };
+
+  byte button[6] = { 30, 31, 32, 33, 34, 35 };
+  byte led[6] = { 22, 23, 24, 25, 26, 27 };
+  byte pot[6] = { A10, A11, A12, A13, A14, A15 };
+};
